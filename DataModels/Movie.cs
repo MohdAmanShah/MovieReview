7f381730128e60899e6609d73d7d1b4cf2d60f35
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-02-25 10:21:56 +0530

    Add project files.

---
+++ DataModels/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class Movie
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ DataModels/ReviewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class ReviewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateOnly Date { get; set; }
+        public List<String> DownloadLinks { get; set; }
+        public string Body { get; set; }
+        public int Upvotes { get; set; }
+    }
+}
